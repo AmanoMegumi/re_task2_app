e76e7e3ea8f3d1c2da2fe21295ae0ef375531f68
Megumi <suz34000@gmail.com>
2022-06-15 10:46:58 +0900

    質問するため

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,10 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  ## before_action :configure_permitted_parameters, if: :devise_controller?
+
+  ## private
+  ## def configure_permitted_parameters
+  ##    devise_parameter_sanitizer.permit(:name, :email, :password, :password_confirmation)
+  ## end
 end
